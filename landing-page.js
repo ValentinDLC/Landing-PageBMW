e4da63f23d7b251bfea7077603b67f3c72b12b5e
Dardenne <Valentin.dardenne9@gmail.com>
2021-06-04 07:28:01 +0200

    mise en site

---
+++ landing-page.js
@@ -0,0 +1,10 @@
+function divart(id){
+
+    var div = document.getElementById(id);
+
+    if(div.style.display == 'none'){
+        div.style.display = 'block';
+    } else {
+        div.style.display = 'none';
+    } 
+} 
